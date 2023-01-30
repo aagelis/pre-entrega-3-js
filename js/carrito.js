0f5b95d7f0ca54dad5a98f0706ce5fa854dced23
aagelis <axelgelis@outlook.com>
2023-01-30 02:55:11 -0300

    Entrega de trabajo

---
+++ js/carrito.js
@@ -0,0 +1,85 @@
+const pintarCarrito = () =>{
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+    <h1 class="modal-header-title">Carrito</h1>
+    `;
+    modalContainer.append(modalHeader);
+    const modalbutton = document.createElement("h1");
+    modalbutton.innerText = "X";
+    modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    })
+    modalHeader.append(modalbutton);
+
+    carrito.forEach ((product) => {
+        let carritoContent = document.createElement("div")
+    carritoContent.className = "modal-content"
+    carritoContent.innerHTML= `
+        <img src= "${product.img}">
+        <h3>${product.nombre}</h3>
+        <p>${product.precio} $</p>
+        <span class="restar"> - </span>
+        <p>Cantidad: ${product.cantidad}</p>
+        <span class="sumar"> + </span>
+        <p> Total: ${product.cantidad * product.precio}</p>
+        <span class="delete-product"> X </span>
+    `;
+    modalContainer.append(carritoContent)
+
+    let restar = carritoContent.querySelector(".restar")
+    restar.addEventListener("click", () => {
+        if(product.cantidad !==1)
+        product.cantidad--;
+        saveLocal();
+        pintarCarrito();
+    })
+
+    let sumar = carritoContent.querySelector(".sumar")
+    sumar.addEventListener("click", () => {
+        product.cantidad++;
+        saveLocal();
+        pintarCarrito();
+    })
+    
+    let eliminar = carritoContent.querySelector(".delete-product");
+    eliminar.addEventListener("click", () => {
+        eliminarProducto(product.id);
+    })
+    });
+
+
+
+    const total = carrito.reduce((acc,el) => acc + el.precio * el.cantidad, 0);
+
+    const totalCompra = document.createElement("div")
+    totalCompra.className = "total-content"
+    totalCompra.innerHTML = `total a pagar : ${total} $`
+    modalContainer.append(totalCompra);
+};
+
+verCarrito.addEventListener("click", pintarCarrito);
+
+const eliminarProducto = (id) => {
+    const buscarId = carrito.find((element) => element.id === id);
+
+    carrito = carrito.filter ((carritoId) => {
+        return carritoId !== buscarId;
+    })
+    carritoContador();
+    saveLocal()
+    pintarCarrito();
+}
+
+const carritoContador = () => {
+    cantidadCarrito.style.display = "block";
+    const carritoLength = carrito.length;
+    localStorage.setItem("carritoLength", JSON.stringify(carritoLength));
+    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"));
+}
+
+carritoContador();
---
+++ js/product.js
@@ -0,0 +1,34 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Mouse",
+        precio: 5000,
+        img:
+        "https://m.media-amazon.com/images/I/61O-tBIkcfS._AC_SL1500_.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 2,
+        nombre: "Teclado",
+        precio: 7000,
+        img:
+        "https://s3-sa-east-1.amazonaws.com/saasargentina/U7DRqRfkhk5o0J3ERB86/imagen",
+        cantidad: 1,
+    },
+    {
+        id: 3,
+        nombre: "Auriculares",
+        precio: 10000,
+        img:
+        "https://http2.mlstatic.com/D_NQ_NP_738955-MLA40252400386_122019-O.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 4,
+        nombre: "Microfono",
+        precio: 15000,
+        img:
+        "https://mcegamer.com/wp-content/uploads/2020/11/FYRU-1.jpg",
+        cantidad: 1,
+    },
+];
